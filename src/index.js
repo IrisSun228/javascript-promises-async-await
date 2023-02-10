5d9973099fc9a1b27c35b9db8722aa49969b61fb
Iris Sun <irissun@live.com>
2023-02-09 20:54:55 -0500

    Section 2 Step 6 - Add return statement to fetchMovies()

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ const movies = require("./data/movies.json");
 
 export function fetchMovies() {
     const resolveFunction = () => movies;
+    return fetchWithTimeout(1000).then(resolveFunction);
 }
