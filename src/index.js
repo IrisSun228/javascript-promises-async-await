e5c8b001d4f6835658b109362a05abe3194ae67b
Iris Sun <irissun@live.com>
2023-02-09 21:14:11 -0500

    Section 3 Step 8 - Log results of getBooksAndMovies()

--- src/index.js
+++ src/index.js
@@ -9,3 +9,6 @@ function getBooksAndMovies() {
         }))
         .catch(error => console.log("Error fetching books and movies", error));
 }
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(results => {console.log('getBooksAndMoviesPromise', results)});
