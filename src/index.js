cee420863e4c082bef77313749ec592faff57aa1
Iris Sun <irissun@live.com>
2023-02-09 21:12:32 -0500

    Section 3 Step 7 - Fetch both books and movies with Promise.all()

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import {fetchWithTimeout, fetchMovies, fetchBooks} from "./services";
 const movies = require("./data/movies.json");
 
+function getBooksAndMovies() {
+    return Promise.all([fetchBooks(), fetchMovies()])
+        .then(([books, movies ]) => ({
+            books,
+            movies
+        }))
+        .catch(error => console.log("Error fetching books and movies", error));
+}
