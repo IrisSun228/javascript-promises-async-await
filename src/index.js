4b1c2576cbf2d1028afaebc2bddd0d3ca5e01a09
Iris Sun <irissun@live.com>
2023-02-10 19:04:10 -0500

    Revert to before Section 7 Step 3

--- src/index.js
+++ src/index.js
@@ -30,8 +30,12 @@ async function getBooksAndMoviesAsync() {
 }
 
 async function getBooksOrMoviesAsync() {
-    const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
-    return values;
+    try {
+        const values = Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+        return values;
+    } catch (error) {
+        console.error("Error waiting for the promise race", error);
+    }
 }
 
 getBooksAndMoviesAsync().then(results => {
